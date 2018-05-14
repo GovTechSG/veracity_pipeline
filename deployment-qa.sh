4b62fa56fecba9f657adf13f7fceae809f2ee706
Wynn Chen <wynn987@gmail.com>
2018-05-14 17:13:22 +0800

    clean up docker scripts

--- deployment-qa.sh
+++ deployment-qa.sh
@@ -1,9 +1,9 @@
 docker-compose -f docker-compose-qa.yml down
-docker-compose -f docker-compose-qa.yml pull qa
-docker-compose -f docker-compose-qa.yml pull sandbox
+docker-compose -f docker-compose-qa.yml pull
 docker-compose -f docker-compose-qa.yml up -d --build
 docker-compose -f docker-compose-qa.yml run qa rake db:environment:set RAILS_ENV=qa db:schema:load db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 docker-compose -f docker-compose-qa.yml run sandbox db:environment:set RAILS_ENV=production rake db:migrate
-docker-compose -f docker-compose-qa.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" sandbox
-docker-compose -f docker-compose-qa.yml run -d --publish 3002:3000 --entrypoint "sh /app/docker-onrun.sh" qa
+docker-compose -f docker-compose-qa.yml run -d --publish 3002:3000 --entrypoint "sh /app/docker-onrun.sh" sandbox
+docker-compose -f docker-compose-qa.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" qa
+docker-compose -f docker-compose-qa.yml run -d --publish 5000:5000 frontend
 docker system prune -a -f
