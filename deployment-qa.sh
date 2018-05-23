50a88c818f1c7d9f1991824a03b129f334d4c57d
Ubuntu <ubuntu@ip-10-7-4-250.ap-southeast-1.compute.internal>
2018-05-23 08:16:40 +0000

    change frontend image

--- deployment-qa.sh
+++ deployment-qa.sh
@@ -4,7 +4,7 @@ echo '---pulling images---'
 docker-compose -f docker-compose-qa.yml pull
 echo '---starting clamd and building images---'
 docker-compose -f docker-compose-qa.yml run qa sh -c "rake db:drop db:create db:schema:load db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
-docker-compose -f docker-compose-qa.yml run -d --publish 3000:3000 --entrypoint "sh /app/docker-onrun.sh" qa
+docker-compose -f docker-compose-qa.yml run -d --publish 3000:3000 --name veracity_backend  --entrypoint "sh /app/docker-onrun.sh" qa
 docker-compose -f docker-compose-qa.yml run sandbox sh -c "rake db:migrate"
 docker-compose -f docker-compose-qa.yml run -d --publish 3002:3000 --entrypoint "sh /app/docker-onrun.sh" sandbox
 sleep 5s
